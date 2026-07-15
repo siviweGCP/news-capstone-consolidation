16d3550e2853a72349f6d35a334320bec561fa6a
Siviwe Ngwilingwili <siviwengwili@gmail.com>
2026-07-15 14:20:46 +0200

    docs: document project URL configuration

--- news_project/urls.py
+++ news_project/urls.py
@@ -1,3 +1,5 @@
+"""Root URL configuration for the Django News Application project."""
+
 from django.contrib import admin
 from django.urls import include, path
 urlpatterns = [path('admin/', admin.site.urls), path('api/', include('news.api_urls')), path('', include('news.urls'))]
